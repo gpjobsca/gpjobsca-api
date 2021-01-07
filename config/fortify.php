d80275b0d52b98254fd38fc8897ffa38945f631f
Todd Smith-Salter <todd@precisepixels.ca>
2021-01-07 16:10:57 -0700

    Registration and passwords working

--- config/fortify.php
+++ config/fortify.php
@@ -132,10 +132,10 @@ return [
 
     'features' => [
         Features::registration(),
-        // Features::resetPasswords(),
-        // Features::emailVerification(),
+        Features::resetPasswords(),
+        Features::emailVerification(),
         // Features::updateProfileInformation(),
-        // Features::updatePasswords(),
+        Features::updatePasswords(),
         // Features::twoFactorAuthentication([
         //     'confirmPassword' => true,
         // ]),
